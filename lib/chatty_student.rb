36e73ff175f50a034d309e1f33d9b94a40d6194e
Kiri Um <kirirotha@gmail.com>
2020-07-21 20:53:04 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,16 +1,16 @@
 
 require_relative './student.rb'
 class ChattyStudent < Student
-  attr_accessor :index
+
   def hello
+    super
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
-  def raise_hand
-    index = 0
-      super
-       index ++
-
-  end
+  # def raise_hand
+  #   super
+  #   super
+  # 
+  # end
 end
